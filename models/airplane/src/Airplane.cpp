ac25f6180dcffd4a22b3e4abdca4acfa046185c9
EndDescender <bahram.mohmand123@gmail.com>
2022-12-23 23:00:31 -0600

    please

--- models/airplane/src/Airplane.cpp
+++ models/airplane/src/Airplane.cpp
@@ -110,8 +110,8 @@ int Airplane::shutdown()
     printf( "========================================\n");
     printf( "      Cannon Ball State at Shutdown     \n");
     printf( "t = %g\n", t);
-    printf( "pos = [%.9f, %.9f]\n", C->pos[0], C->pos[1]);
-    printf( "vel = [%.9f, %.9f]\n", C->vel[0], C->vel[1]);
+    printf( "pos = [%.9f, %.9f]\n", pos[0], pos[1]);
+    printf( "vel = [%.9f, %.9f]\n", vel[0], vel[1]);
     printf( "========================================\n");
     return 0 ;
 }
