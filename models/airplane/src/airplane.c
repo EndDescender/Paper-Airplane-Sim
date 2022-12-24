b533b7033319ad9ee25d5dadacfe5d5aeba42e8b
EndDescender <bahram.mohmand123@gmail.com>
2022-12-23 20:49:46 -0600

    Changed files from C++ to C

---
+++ models/airplane/src/airplane.c
@@ -0,0 +1,10 @@
+/**********************************
+PURPOSE: (Sets initial variables for paper aiprlane)
+***********************************/
+
+#include "../include/airplane.h"
+
+int airplane_default_data(AIRPLANE* plane)
+{
+    
+}
